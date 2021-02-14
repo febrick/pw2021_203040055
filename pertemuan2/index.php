cf48ba475b2fa73c5aea22d020a4d8b62e81448e
febrick <febrick.hariyanto@gmail.com>
2021-02-14 21:59:08 +0700

    menyelesaikan tugas pertemuan2

---
+++ pertemuan2/index.php
@@ -0,0 +1,64 @@
+<?php
+/*
+	Febrick Hariyanto
+	203040055
+	github.com/febrick/pw2021_203040055
+	Pertemuan 2 (14 Februari 2021)
+	Materi minggu ini mempelajari mengenai penggunaan sintaks PHP Dasar
+*/
+
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Febrick Hariyanto";
+// echo 'Halo, nama saya $nama';
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y
+
+// penggabung string / concatenation / contact
+// .
+// $nama_depan = "Febrick";
+// $nama_belakang = "Hariyanto";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Febrick";
+// $nama .= " ";
+// $nama .= "Hariyanto";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+
+?>
