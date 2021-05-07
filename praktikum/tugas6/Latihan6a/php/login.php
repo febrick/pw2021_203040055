e83fd986f2ed044d50919ff2a82e3aa32e94785b
febrick <febrick.hariyanto@gmail.com>
2021-05-07 13:46:46 +0700

    tambahan

--- praktikum/tugas6/Latihan6a/php/login.php
+++ praktikum/tugas6/Latihan6a/php/login.php
@@ -32,7 +32,7 @@ if(isset($_POST['submit'])){
 			header("Location: admin.php");
 			die;
 		}
-		header("Location: ../index.php")
+		header("Location: ../index.php");
 	}
 }
 ?>
--- praktikum/tugas6/Latihan6c/php/login.php
+++ praktikum/tugas6/Latihan6c/php/login.php
@@ -55,7 +55,7 @@ if(isset($_POST['submit'])){
             header("Location: admin.php");
             die;
         }
-		header("Location: ../index.php")
+		header("Location: ../index.php");
 	}
 }
 ?>
